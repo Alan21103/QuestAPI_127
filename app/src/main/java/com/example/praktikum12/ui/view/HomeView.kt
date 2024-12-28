cf82b85bebc1d2c732ca958f40ac2d49f0f3161f
Alan21103 <syahlansaputra03@gmail.com>
2024-12-29 00:18:07 +0700

    Menyusun HomeStatus untuk Menangani Status Loading, Success, dan Error

--- app/src/main/java/com/example/praktikum12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/praktikum12/ui/view/HomeView.kt
@@ -1,5 +1,8 @@
 package com.example.praktikum12.ui.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -8,6 +11,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +19,9 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum12.Navigation.DestinasiNavigasi
+import com.example.praktikum12.model.Mahasiswa
 import com.example.praktikum12.ui.customwidget.CostumeTopAppBar
+import com.example.praktikum12.ui.viewmodel.HomeUiState
 import com.example.praktikum12.ui.viewmodel.HomeViewModel
 
 object DestinasiHome : DestinasiNavigasi {
@@ -65,6 +71,37 @@ fun HomeScreen(
     }
 }
 
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+) {
+    when (homeUiState) {
+        is HomeUiState.Loading -> {
+            OnLoading(modifier = modifier.fillMaxSize())
+        }
+        is HomeUiState.Success -> {
+            if (homeUiState.mahasiswa.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Mahasiswa")
+                }
+            } else {
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { onDetailClick(it.nim) },
+                    onDeleteClick = { onDeleteClick(it) }
+                )
+            }
+        }
+        is HomeUiState.Error -> {
+            OnError(retryAction, modifier = modifier.fillMaxSize())
+        }
+    }
+}
 
 
 
