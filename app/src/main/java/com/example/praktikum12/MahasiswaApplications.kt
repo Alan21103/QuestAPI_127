0c18e8d1e7d871e9a1c2041c2c71807efec26a06
Alan21103 <syahlansaputra03@gmail.com>
2024-12-28 23:19:27 +0700

    Membuat class mahasiswa application

--- app/src/main/java/com/example/praktikum12/MahasiswaApplications.kt
+++ app/src/main/java/com/example/praktikum12/MahasiswaApplications.kt
@@ -1,7 +1,13 @@
 package com.example.praktikum12
 
 import android.app.Application
+import com.example.praktikum12.repository.AppContainer
+import com.example.praktikum12.repository.MahasiswaContainer
 
 class MahasiswaApplications:Application() {
-    lateinit var cont
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container= MahasiswaContainer()
+    }
 }
