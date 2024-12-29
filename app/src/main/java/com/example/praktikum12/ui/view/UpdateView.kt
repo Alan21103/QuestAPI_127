1be97624d16201c8de6f30af9c90fc10eccbd39e
Alan21103 <syahlansaputra03@gmail.com>
2024-12-30 01:14:32 +0700

    Menambahkan formulir input dengan validasi sederhana untuk pembaruan data mahasiswa

--- app/src/main/java/com/example/praktikum12/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/praktikum12/ui/view/UpdateView.kt
@@ -1,21 +1,31 @@
 package com.example.praktikum12.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum12.Navigation.DestinasiNavigasi
 import com.example.praktikum12.ui.customwidget.CostumeTopAppBar
 import com.example.praktikum12.ui.viewmodel.PenyediaViewModel
+import com.example.praktikum12.ui.viewmodel.UpdateUiEvent
+import com.example.praktikum12.ui.viewmodel.UpdateUiState
 import com.example.praktikum12.ui.viewmodel.UpdateViewModel
 import kotlinx.coroutines.launch
 
@@ -63,6 +73,105 @@ fun UpdateView(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onSiswaValueChange: (UpdateUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Update")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = updateUiEvent.nama,
+            onValueChange = { onValueChange(updateUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.nim,
+            onValueChange = { onValueChange(updateUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.alamat,
+            onValueChange = { onValueChange(updateUiEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.jenis_kelamin,
+            onValueChange = { onValueChange(updateUiEvent.copy(jenis_kelamin = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.kelas,
+            onValueChange = { onValueChange(updateUiEvent.copy(kelas = it)) },
+            label = { Text("Kelas") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.angkatan,
+            onValueChange = { onValueChange(updateUiEvent.copy(angkatan = it)) },
+            label = { Text("Angkatan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
         )
     }
 }
