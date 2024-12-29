b1147bfe88eb493571a6e6e1f369f7535cdb3153
Alan21103 <syahlansaputra03@gmail.com>
2024-12-30 05:30:03 +0700

    Membuat penyedia view model dan menambahkan detail view model dan update view model

---
+++ app/src/main/java/com/example/praktikum12/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.praktikum12.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum12.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+        initializer {
+            DetailViewModel(aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+        initializer {
+            UpdateViewModel(aplikasiMahasiswa().container.mahasiswaRepository)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
