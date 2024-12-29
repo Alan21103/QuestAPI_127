c8557940b4a8f0061f12a430f0fe4a32cd1708c3
Alan21103 <syahlansaputra03@gmail.com>
2024-12-30 01:02:48 +0700

    Menambah fungsi object destinasi update untuk pindah halaman ke update view

---
+++ app/src/main/java/com/example/praktikum12/ui/view/UpdateView.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum12.ui.view
+
+import com.example.praktikum12.Navigation.DestinasiNavigasi
+
+object DestinasiUpdate : DestinasiNavigasi {
+    override val route = "item_update"
+    override val titleRes = "Update Mahasiswa"
+}
+
+
